1ca16325733b5a019e10ff6b692c9dd816699ba0
gowthambalachandhiran <inboxgowtham@yahoo.com>
2022-09-18 14:55:49 +0000

    Initial commit

---
+++ Assets/Scripts/Superman/SupermanUpDown.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupermanUpDown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //adjust this to change speed
+    float speed = 5f;
+    //adjust this to change how high it goes
+    float height = 0.5f;
+
+    void Update()
+    {
+        //get the objects current position and put it in a variable so we can access it later with less code
+        Vector3 pos = transform.position;
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed);
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
+    }
+
+}
